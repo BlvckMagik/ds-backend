5055f60db5e93f42c2ce9d3adb5c51a396f34e5e
Leeroy <dandol777@gmail.com>
2025-02-11 20:57:49 +0200

    Add chat retrieval endpoints for unique chats and user messages

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Controller, Post, Body, Param, Get } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { TelegramService } from '../telegram/telegram.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Message, MessageDocument } from './schemas/message.schema';
 
 @Controller('chat')
 export class ChatController {
   constructor(
     private readonly chatService: ChatService,
     private readonly telegramService: TelegramService,
+    @InjectModel(Message.name) private messageModel: Model<MessageDocument>,
   ) {}
 
   @Post('send')
@@ -46,6 +51,49 @@ export class ChatController {
       return { response };
     } catch (error) {
       console.error('Error in chat controller:', error);
+      throw error;
+    }
+  }
+
+  @Get('get-unique-chats')
+  async getUniqueChats() {
+    try {
+      const uniqueChats = await this.messageModel.aggregate([
+        {
+          $sort: { createdAt: -1 }, // Сортуємо за часом створення (останні повідомлення першими)
+        },
+        {
+          $group: {
+            _id: '$userId',
+            lastMessage: { $first: '$$ROOT' },
+          },
+        },
+        {
+          $replaceRoot: { newRoot: '$lastMessage' },
+        },
+      ]);
+
+      return uniqueChats;
+    } catch (error) {
+      console.error('Помилка при отриманні чатів:', error);
+      throw error;
+    }
+  }
+
+  @Get(':userId')
+  async getUserMessages(@Param('userId') userId: string) {
+    try {
+      const messages = await this.messageModel
+        .find({ userId: Number(userId) })
+        .sort({ createdAt: -1 })
+        .exec();
+
+      return messages;
+    } catch (error) {
+      console.error(
+        `Помилка при отриманні повідомлень користувача ${userId}:`,
+        error,
+      );
       throw error;
     }
   }
